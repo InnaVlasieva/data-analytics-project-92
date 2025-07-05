c3ca0f997f1fb518fc624fe8bd5eeee3ac080179
InnaVlasieva <inna.vlasieva@gmail.com>
2025-07-05 19:51:52 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -41,4 +41,15 @@ GROUP BY  e.first_name || ' ' || e.last_name,
 TO_CHAR(s.sale_date, 'day'), EXTRACT(ISODOW FROM s.sale_date)
 ORDER BY EXTRACT(ISODOW FROM s.sale_date),  e.first_name || ' ' || e.last_name;
 
+-- количество покупателей в разных возрастных группах - age_groups
+SELECT 
+    CASE 
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        ELSE '40+'
+    END AS age_category,
+    COUNT(customer_id) AS age_count
+FROM customers
+GROUP BY age_category
+order by age_category;
 
