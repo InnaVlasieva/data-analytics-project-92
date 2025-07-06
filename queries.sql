fdf11124661f95ce74188ae02c0dbdacd18aaac0
InnaVlasieva <inna.vlasieva@gmail.com>
2025-07-06 17:03:41 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,12 +3,13 @@ from customers c;
 -- считает общее количество покупателей из таблицы customers
 
 --  отчет с продавцами у которых наибольшая выручка - top_10_total_income.csv 
-select e.first_name || ' ' || e.last_name AS seller,
-     COUNT(s.sales_id) AS operations,
-     FLOOR (SUM(s.quantity * p.price)) AS income
-from employees e 
-inner join sales s on e.employee_id = s.sales_person_id
-inner join products p  on s.product_id = p.product_id
+select
+    e.first_name || ' ' || e.last_name as seller,
+    COUNT(s.sales_id) as operations,
+    FLOOR(SUM(s.quantity * p.price)) as income
+from employees as e
+inner join sales as s on e.employee_id = s.sales_person_id
+inner join products as p on s.product_id = p.product_id
 group by e.first_name || ' ' || e.last_name
 order by income desc limit 10;
 
