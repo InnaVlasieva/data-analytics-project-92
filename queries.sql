c8c8840147abe382158c24eae7b101166752787f
InnaVlasieva <inna.vlasieva@gmail.com>
2025-09-22 20:01:33 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,12 +1,12 @@
-select COUNT(customer_id) as customers_count
+select count(customer_id) as customers_count
 from customers;
 -- считает общее количество покупателей из таблицы customers
 
 --  отчет с продавцами у которых наибольшая выручка - top_10_total_income.csv 
 select
     e.first_name || ' ' || e.last_name as seller,
-    COUNT(s.sales_id) as operations,
-    FLOOR(SUM(s.quantity * p.price)) as income
+    count(s.sales_id) as operations,
+    floor(sum(s.quantity * p.price)) as income
 from employees as e
 inner join sales as s on e.employee_id = s.sales_person_id
 inner join products as p on s.product_id = p.product_id
@@ -14,85 +14,85 @@ group by e.first_name || ' ' || e.last_name
 order by income desc limit 10;
 
 --отчет с продавцами, чья выручка ниже средней выручки всех продавцов - lowest_average_income
-SELECT
-    e.first_name || ' ' || e.last_name AS seller,
-    FLOOR(AVG(s.quantity * p.price)) AS average_income
-FROM employees AS e
-INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
-INNER JOIN products AS p ON s.product_id = p.product_id
-GROUP BY e.first_name, e.last_name
-HAVING
-    AVG(s.quantity * p.price) < (
-        SELECT AVG(sub.total_income)
-        FROM (
-            SELECT AVG(s2.quantity * p2.price) AS total_income
-            FROM employees AS e2
-            INNER JOIN sales AS s2 ON e2.employee_id = s2.sales_person_id
-            INNER JOIN products AS p2 ON s2.product_id = p2.product_id
-            GROUP BY e2.employee_id
-        ) AS sub
+select
+    e.first_name || ' ' || e.last_name as seller,
+    floor(avg(s.quantity * p.price)) as average_income
+from employees as e
+inner join sales as s on e.employee_id = s.sales_person_id
+inner join products as p on s.product_id = p.product_id
+group by e.first_name, e.last_name
+having
+    avg(s.quantity * p.price) < (
+        select avg(sub.total_income)
+        from (
+            select avg(s2.quantity * p2.price) as total_income
+            from employees as e2
+            inner join sales as s2 on e2.employee_id = s2.sales_person_id
+            inner join products as p2 on s2.product_id = p2.product_id
+            group by e2.employee_id
+        ) as sub
     )
-ORDER BY average_income ASC;
+order by average_income asc;
 
 --Отчет с данными по выручке по каждому продавцу и дню недели - day_of_the_week_income
-SELECT
-    e.first_name || ' ' || e.last_name AS seller,
-    TO_CHAR(s.sale_date, 'day') AS day_of_week,
-    FLOOR(SUM(s.quantity * p.price)) AS income
-FROM employees AS e
-INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
-INNER JOIN products AS p ON s.product_id = p.product_id
-GROUP BY
+select
+    e.first_name || ' ' || e.last_name as seller,
+    to_char(s.sale_date, 'day') as day_of_week,
+    floor(sum(s.quantity * p.price)) as income
+from employees as e
+inner join sales Aas s on e.employee_id = s.sales_person_id
+inner join products as p on s.product_id = p.product_id
+group by 
     e.first_name || ' ' || e.last_name,
-    TO_CHAR(s.sale_date, 'day'), EXTRACT(ISODOW FROM s.sale_date)
-ORDER BY EXTRACT(ISODOW FROM s.sale_date), e.first_name || ' ' || e.last_name;
+    to_char(s.sale_date, 'day'), extract(ISODOW from s.sale_date)
+order by extract(ISODOW from s.sale_date), e.first_name || ' ' || e.last_name;
 
 -- количество покупателей в разных возрастных группах - age_groups
-SELECT
-    CASE
-        WHEN age BETWEEN 16 AND 25 THEN '16-25'
-        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+select
+    case
+        when age between 16 and 25 then '16-25'
+        when age between 26 and 40 then'26-40'
         ELSE '40+'
-    END AS age_category,
-    COUNT(customer_id) AS age_count
-FROM customers
-GROUP BY age_category
-ORDER BY age_category;
+    end as age_category,
+    count(customer_id) as age_count
+from customers
+group by  age_category
+order by age_category;
 
 -- данные по количеству уникальных покупателей и выручке, которую они принесли-customers_by_month
-SELECT
-    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
-    COUNT(DISTINCT c.customer_id) AS total_customers,
-    FLOOR(SUM(s.quantity * p.price)) AS income
-FROM employees AS e
-INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
-INNER JOIN products AS p ON s.product_id = p.product_id
-INNER JOIN customers AS c ON s.customer_id = c.customer_id
-GROUP BY TO_CHAR(s.sale_date, 'YYYY-MM')
-ORDER BY selling_month ASC;
+select
+    to_char(s.sale_date, 'YYYY-MM') as selling_month,
+    count(distinct c.customer_id) as total_customers,
+    floor(sum(s.quantity * p.price)) as income
+from employees as e
+inner join sales as s on e.employee_id = s.sales_person_id
+inner join products as p on s.product_id = p.product_id
+inner join customers as c on s.customer_id = c.customer_id
+group by to_char(s.sale_date, 'YYYY-MM')
+order by selling_month ASC;
 
 -- с покупателями первая покупка которых пришлась на время проведения специальных акций - special_offer
-WITH price_null AS (
-    SELECT product_id
-    FROM products
-    WHERE price = 0
+with price_null as (
+    select product_id
+    from products
+    where price = 0
 ),
-first_null_sales AS (
-    SELECT
+first_null_sales as (
+    select
         s.customer_id,
-        MIN(s.sale_date) AS first_null_sale_date
-    FROM sales s
-    INNER JOIN price_null pn ON s.product_id = pn.product_id
-    GROUP BY s.customer_id
+        min(s.sale_date) as first_null_sale_date
+    from sales s
+    inner joinprice_null pn on s.product_id = pn.product_id
+    group by s.customer_id
 )
-SELECT 
-    c.first_name || ' ' || c.last_name AS customer,
-    TO_CHAR(s.sale_date, 'YYYY-MM-DD') AS sale_date,
-    e.first_name || ' ' || e.last_name AS seller
-FROM sales s
-INNER JOIN products p ON s.product_id = p.product_id
-INNER JOIN customers c ON s.customer_id = c.customer_id
-INNER JOIN employees e ON s.sales_person_id = e.employee_id
-INNER JOIN first_null_sales fs ON s.customer_id = fs.customer_id AND s.sale_date = fs.first_null_sale_date
+select
+    c.first_name || ' ' || c.last_name as customer,
+    to_char(s.sale_date, 'YYYY-MM-DD') as sale_date,
+    e.first_name || ' ' || e.last_name as seller
+from sales s
+inner join products p on s.product_id = p.product_id
+inner join customers c on s.customer_id = c.customer_id
+inner join employees e on s.sales_person_id = e.employee_id
+inner join first_null_sales fs on s.customer_id = fs.customer_id and  s.sale_date = fs.first_null_sale_date
 group by s.sale_date, c.first_name || ' ' || c.last_name, e.first_name || ' ' || e.last_name
-ORDER BY customer;
+order by customer;
